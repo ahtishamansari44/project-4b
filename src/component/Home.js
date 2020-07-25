1ddb20c2c07fd2e1d59e2fec51799b1ff2bf4081
ahtishamansari44 <ahtishamansari44@gmail.com>
2020-07-25 21:28:31 +0500

    add project 4B

---
+++ src/component/Home.js
@@ -0,0 +1,272 @@
+import React, {useEffect} from 'react';
+import useWebAnimations, { fadeIn, flip, zoomIn, slideInUp, fadeInLeft } from "@wellyshen/use-web-animations";
+import { makeStyles, Paper, Grid, Container, Typography } from '@material-ui/core';
+import SVG1 from '../svg/svg1.js'
+import SVG3 from '../svg/svg3.js'
+import logo from '../svg/logo.png'
+import AboutImg from '../svg/about-img.png'
+import SVG2 from '../svg/svg2.svg'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
+  const link1 = useWebAnimations();
+  const link2 = useWebAnimations();
+  const link3 = useWebAnimations();
+  const link4 = useWebAnimations();
+  const link5 = useWebAnimations();
+  const image1 = useWebAnimations({ ...zoomIn });
+  const image2 = useWebAnimations({ ...zoomIn });
+  const flip1 = useWebAnimations({ ...flip });
+  const flip2 = useWebAnimations({ ...flip });
+  const flip3 = useWebAnimations({ ...flip });
+  const flip4 = useWebAnimations({ ...flip });
+  const flip5 = useWebAnimations({ ...flip });
+  const flip6 = useWebAnimations({ ...flip });
+  const flip7 = useWebAnimations({ ...flip });
+  const slide1 = useWebAnimations({ ...slideInUp });
+  const slide2 = useWebAnimations({ ...slideInUp });
+  const slide3 = useWebAnimations({ ...slideInUp });
+  const slide4 = useWebAnimations({ ...slideInUp });
+  const slide5 = useWebAnimations({ ...slideInUp });
+  const slide6 = useWebAnimations({ ...slideInUp });
+  const slide7 = useWebAnimations({ ...slideInUp });
+  const slide8 = useWebAnimations({ ...slideInUp });
+  const slide9 = useWebAnimations({ ...slideInUp });
+  const slide10 = useWebAnimations({ ...slideInUp });
+  const fadeL1 = useWebAnimations({ ...fadeInLeft });
+  const fadeL2 = useWebAnimations({ ...fadeInLeft });
+  const fadeL3 = useWebAnimations({ ...fadeInLeft });
+
+
+
+
+  useEffect(() => {
+    document.addEventListener("mouseover", (e) => {
+      link1.animate({
+        keyframes: [ {transform: 'translate(0%, 0)',  borderBottomColor: '#2196F3'}, { offset: 0.8, transform: 'translate(100%, 0)', borderBottomColor: '#fdb811'} ],
+        timing: { duration: 800, 
+          fill: 'forwards' },
+      });
+      link2.animate({
+        keyframes: [ {transform: 'translate(0%, 0)',  borderBottomColor: '#2196F3'}, { offset: 0.8, transform: 'translate(100%, 0)', borderBottomColor: '#fdb811'} ],
+        timing: { duration: 800, 
+          fill: 'forwards' },
+      });
+      link3.animate({
+        keyframes: [ {transform: 'translate(0%, 0)',  borderBottomColor: '#2196F3'}, { offset: 0.8, transform: 'translate(100%, 0)', borderBottomColor: '#fdb811'} ],
+        timing: { duration: 800, 
+          fill: 'forwards' },
+      });
+      link4.animate({
+        keyframes: [ {transform: 'translate(0%, 0)',  borderBottomColor: '#2196F3'}, { offset: 0.8, transform: 'translate(100%, 0)', borderBottomColor: '#fdb811'} ],
+        timing: { duration: 800, 
+          fill: 'forwards' },
+      });
+      link5.animate({
+        keyframes: [ {transform: 'translate(0%, 0)',  borderBottomColor: '#2196F3'}, { offset: 0.8, transform: 'translate(100%, 0)', borderBottomColor: '#fdb811'} ],
+        timing: { duration: 800, 
+          fill: 'forwards' },
+      });
+    });
+  }, [link1.animate, link2.animate, link3.animate, link4.animate, link5.animate]);
+
+  return (
+    <>
+    <Container style={{paddingTop:20, paddingBottom:10}}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={0} className={classes.paper}>
+            <SVG1 />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={0} className={classes.paper}>
+          <Typography className="head-title" variant="h2"  component="h1" ref={slide1.ref} >
+            Scalable extended
+            business office
+            services
+          </Typography>
+          <a href="#" ref={link1.ref} className="link" >Revenue Cycle Management</a>
+          </Paper>
+        </Grid>
+      </Grid>
+      </Container>
+      <Container style={{paddingTop:20, paddingBottom:10}}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={0} className={classes.paper}>
+          <Typography className="sub-title1" variant="h1" component="h1" ref={slide2.ref} >
+            We Are
+          </Typography>
+          <Typography className="title1-content" variant="boby1" component="p" ref={slide3.ref} >
+          A highly specialized Management team with over 20 combined years of experience in the Healthcare industry with proven ability to optimally combine digital technology, skilled resources and efficient business processes to implement creative as well as proven market solutions to suit an ever-evolving market. Our forte has been to successfully tailor our solutions to support your needs whether it is in the space of Consulting, Product Management or Service Delivery
+          </Typography>
+          <a href="#" className="link" ref={link2.ref}>About Us</a>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={0} className={classes.paper}>
+          <img src={AboutImg} ref={image1.ref} alt="about" width={600} />
+          </Paper>
+        </Grid>
+      </Grid>
+      </Container>
+      <Container style={{paddingTop:80, paddingBottom:80}}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Paper style={{position: 'relative'}}  elevation={0} className={classes.paper}>
+           <img src={SVG2} alt="avg2" />
+           <div className="overlay">
+           <Grid container spacing={3}>
+           <Grid item xs={12} sm={4}>
+           <Paper elevation={3} ref={flip1.ref} className="paperBox">
+             <h3>R</h3><br/>
+             <h4>Revenue Cycle Management</h4>
+           </Paper>
+           <Paper elevation={3} ref={flip2.ref} className="paperBox">
+             <h3>S</h3><br/>
+             <h4>Staff
+              Augmentation</h4>
+           </Paper>
+           <Paper elevation={3} ref={flip3.ref} style={{backgroundColor:'#245991'}} className="paperBox">
+             <h3>P</h3><br/>
+             <h4>Payment
+                Posting</h4>
+           </Paper> 
+           </Grid> 
+           <Grid item xs={12} style={{paddingTop:60}} sm={4}>
+           <Paper elevation={3} ref={flip4.ref} style={{backgroundColor:'#003a78',color:'#ffff'}} className="paperBox">
+             <h3>R</h3><br/>
+             <h4>Reason Code
+                Mapping</h4>
+           </Paper>
+           <Paper elevation={3} ref={flip5.ref} className="paperBox">
+             <h3>p</h3><br/>
+             <h4>Product
+              Management</h4>
+           </Paper>
+           <Paper elevation={3} ref={flip6.ref} style={{backgroundColor:'#fdb811', color:'#ffff'}} className="paperBox">
+             <h3>V</h3><br/>
+             <h4>Virtual
+              Assistant</h4>
+           </Paper> 
+           </Grid>
+           <Grid item xs={12} style={{paddingTop:160}} sm={4}>
+           <Paper elevation={3} ref={flip7.ref} className="paperBox">
+             <h3>C</h3><br/>
+             <h4>Correspondence
+                Management</h4>
+           </Paper>
+           </Grid>
+           </Grid>
+           </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+        <Paper elevation={0} className={classes.paper} style={{padding:100}}>
+          <Typography className="sub-title1" variant="h1" component="h1" ref={slide4.ref} >
+          Services.
+          </Typography>
+          <Typography className="title1-content" variant="boby1" component="p" ref={slide5.ref} >
+          We offer customized digital services and scalable operational strategies to prepare your Organization ride through the waves of uncertainty in the form of disruptive technology and government regulations so you can deliver accelerated and cost-effective services for your consumers
+          </Typography>
+          <a href="#" className="link" ref={link3.ref}>Services</a>
+          </Paper>  
+        </Grid>
+      </Grid>
+      </Container>
+      <div style={{backgroundColor:'#003a78'}}>
+      <Container style={{paddingTop:100, paddingBottom:100}}>
+      <Grid container spacing={3}>
+      <Grid item xs={12} sm={12}><Typography style={{color:"#ffff", paddingBottom: 32}} variant="h3" component="h3" ref={slide6.ref} >
+      Economical,<br />
+dynamic and managed business solutions
+          </Typography></Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper elevation={0} style={{backgroundColor:'transparent'}} ref={fadeL1.ref} className={classes.paper}>
+          <Typography style={{color:'#ffff'}} variant="h4" component="h4"  >
+          Improved efficiency
+          </Typography>
+          <Typography style={{color:'#ffff'}} className="title1-content" variant="boby1" component="p"  >
+          By reducing the burden of managing support functions, staffing and day to day operations, your business can concentrate further on core areas.
+          </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper elevation={0} className={classes.paper} ref={fadeL2.ref} style={{backgroundColor:'transparent'}}>
+          <Typography style={{color:'#ffff'}} variant="h4" component="h4"  >
+          Improved efficiency
+          </Typography>
+          <Typography style={{color:'#ffff'}} className="title1-content" variant="boby1" component="p"  >
+          By reducing the burden of managing support functions, staffing and day to day operations, your business can concentrate further on core areas.
+          </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper elevation={0} className={classes.paper} ref={fadeL3.ref} style={{backgroundColor:'transparent'}}>
+          <Typography style={{color:'#ffff'}} variant="h4" component="h4"  >
+          Improved efficiency
+          </Typography>
+          <Typography className="title1-content" variant="boby1" component="p" style={{color:'#ffff'}} >
+          By reducing the burden of managing support functions, staffing and day to day operations, your business can concentrate further on core areas.
+          </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+      </Container>
+      </div>
+      <Container style={{paddingTop:20, paddingBottom:10}}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={0} className={classes.paper}>
+            <SVG3 />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} style={{paddingTop:100}} ref={slide9.ref}>
+        <Typography className="sub-title1" variant="h2" component="h2"  >
+        Talk to our experts
+about your requirements
+          </Typography>
+          <Typography className="title1-content" variant="boby1" component="p" ref={slide10.ref} >
+          Contact us and know more about how our experts can help your business grow.
+          </Typography>
+          <a href="#" className="link" ref={link4.ref}>Enquire Now</a>
+        </Grid>
+      </Grid>
+      </Container>
+      <div style={{backgroundColor:'#003a78'}}>
+      <Container style={{paddingTop:100, paddingBottom:100}}>
+      <Grid container spacing={3}>
+      <Grid item xs={12} sm={3}></Grid>  
+      <Grid item xs={12} sm={6} style={{textAlign:'center'}}>
+        <img src={logo} alt="logo" ref={image2.ref} />
+        <Typography style={{color:"#ffff", paddingBottom: 32, textAlign:'center', marginTop:32}} variant="h3" component="h3" ref={slide7.ref} >
+      Care To Join Us?
+          </Typography>
+          <Typography style={{color:'#ffff', textAlign:'center'}} variant="h6" component="h6" ref={slide8.ref} >
+          Embark on an exciting career with us to learn and grow alongside the best in the industry
+          </Typography>
+          <a href="#" className="link" ref={link5.ref}>View Current Openning</a>
+          </Grid>
+
+          <Grid item xs={12} sm={3}></Grid>
+       
+       
+      </Grid>
+      </Container>
+      </div>
+
+    </>
+  );
+}
---
+++ src/component/Navbar.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import { fade, makeStyles, AppBar, Toolbar, IconButton, Typography, InputBase, Badge, MenuItem, Menu } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import logo from '../svg/logo.png'
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <p>Home</p>
+      </MenuItem>
+      <MenuItem>
+        <p>About Us</p>
+      </MenuItem>
+      <MenuItem>
+        <p>Services</p>
+      </MenuItem>
+      <MenuItem>
+        <p>Career</p>
+      </MenuItem>
+      <MenuItem>
+        <p>Contact</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+        
+        <img src={logo} alt="logo" />
+          <Typography className={classes.title} variant="h6" noWrap>
+            Zapare Technologies
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+          <a href="#" className="tab">Home</a>
+          <a href="#" className="tab">About Us</a>
+          <a href="#" className="tab">Services</a>
+          <a href="#" className="tab">Career</a>
+          <a href="#" className="tab">Contact</a>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
+}
